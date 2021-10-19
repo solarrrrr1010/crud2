92de8aefe87f00d9da6cb64824fd0dea3102df59
kimbongcheol <solarrrrr1010@gmail.com>
2021-10-19 19:35:37 +0900

    과제1 두 번째

--- owners/models.py
+++ owners/models.py
@@ -8,6 +8,7 @@ class Owner(models.Model):
     name = models.CharField(max_length=45)
     email = models.CharField(max_length=300)
     age = models.IntegerField()
+    #dog = models.ForeignKey('Dog', on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'owners'
--- owners/views.py
+++ owners/views.py
@@ -1,4 +1,5 @@
 import json
+from os import name
 
 from django.views import View
 from django.http import HttpResponse, JsonResponse
@@ -8,12 +9,32 @@ from .models import Owner, Dog
 class OwnerView(View):
     def get(self, request):
         # 클라이언트의 요청을 처리할 수 있는 로직 작성
-        Owner.objects.get(
-            name=["name"], 
-            email=["email"],
-            age=["age"]
-            )        
-        return JsonResponse({"message" : "SUCCESS"}, status=200)
+        owners = Owner.objects.all()
+        result = []
+        
+        
+        for owner in owners:
+            dogs = list(Dog.objects.filter(owner=owner).values("name", "age")) #owner.dog_set.all()
+            #dogs_list = []
+            #for dog in dogs:
+            #    dogs_list.append(
+            #        {
+            #            "name" : dog.name,
+            #            "age" : dog.age
+            #        }
+            #    )
+
+
+            result.append(
+                {
+                    "name" : owner.name,
+                    "email" : owner.email,
+                    "age" : owner.age,
+                    "dog" : dogs #dogs_list
+                }
+            )
+               
+        return JsonResponse({"owners" : result}, status=200)
 
     def post(self, request):
         input_data = json.loads(request.body)
@@ -21,6 +42,7 @@ class OwnerView(View):
             name=input_data["name"], 
             email=input_data["email"],
             age=input_data["age"]
+            
             )
 
         return JsonResponse({"message" : "SUCCESS"}, status=201)
@@ -28,14 +50,22 @@ class OwnerView(View):
 
 class DogView(View):
     def get(self, request):
-        # 클라이언트의 요청을 처리할 수 있는 로직 작성
-        Dog.objects.get(
-    
-                )
+        dogs = Dog.objects.all()
+        
+
+        result = []
+        for dog in dogs:
+            result.append(
+                {
+                    "name" : dog.name,
+                    "age" : dog.age,
+                    "owner" : dog.owner.name
+            
+                }
                 
-        
-        
-        return JsonResponse({"message" : "SUCCESS"}, status=200)
+            )
+         
+        return JsonResponse({"dogs" : result}, status=200)
 
     def post(self, request):
         input_data = json.loads(request.body)
@@ -43,6 +73,8 @@ class DogView(View):
             name=input_data["name"],
             age=input_data["age"],
             owner_id=input_data["owner_id"]
+            # owner = input_data.objects.get["owner_id"] 
+            # >> post 할 때도 owner_id에 숫자 말고 이름을 직접 넣어줘야 한다.
         )
 
         return JsonResponse({"message" : "SUCCESS"}, status=201)
